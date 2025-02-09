28dd425c8aede6b23e5a22020a3b9f69ba8f8cfd
Võ Quý Hoàng Long <127716758+longvoquy@users.noreply.github.com>
2025-02-09 23:24:09 +0700

    merge main p2

---
+++ Frontend/booking-platform/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LoginPage } from "./views/login/LoginPage"
+import { HomePage } from "./views/home/HomePage"
+import { HistoryTransaction } from "./views/transaction/HistoryTransaction"
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/transaction" element={<HistoryTransaction/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
